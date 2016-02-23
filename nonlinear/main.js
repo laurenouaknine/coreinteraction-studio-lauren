f473a98341743d81197fdaa85adaf7c2eb95b271
Lauren Ouaknine <ouakl621@newschool.edu>
2016-02-23 09:17:27 -0500

    Linear and Non-Linear Press Release

---
+++ nonlinear/main.js
@@ -0,0 +1,46 @@
+
+//naming function then passing it to set interval
+setInterval(
+
+	function(){
+		++blur;
+		}//end of function
+
+
+	,1000)//end of setInterval
+console.log(setInterval);
+
+
+var elem = document.getElementById("body");
+var blur = 0;
+	console.log(blur);
+
+
+
+
+var frame = function (event){
+	if (blur > 30){
+		blur = -5;
+	} //end of if 
+	else {
+		blur++;
+		document.getElementById('body').style.webkitFilter = 'blur(' + (blur/12) + 'px)';
+	}//end of else
+
+	console.log(frame);	
+}//end of function
+
+
+var id = setInterval(frame, 1000);
+	console.log(id);
+
+
+
+
+//-add the mouse over event
+
+
+
+
+
+
